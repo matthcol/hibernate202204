f3d25871a7d4ecec646ca9558d7586f9f26ce24c
matthcol <matthias.colin@gmail.com>
2022-04-05 15:35:15 +0200

    test mapping asso directed, played movies

--- movieapi/src/test/java/org/example/movieapi/model/PeopleTest.java
+++ movieapi/src/test/java/org/example/movieapi/model/PeopleTest.java
@@ -8,7 +8,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Date;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,5 +46,23 @@ class PeopleTest {
         assertEquals(1968, peopleRead.getBirthYear());
     }
 
+    @Rollback(false)
+    @Test
+    void testPersistDirectedPlayedMovies(){
+        var clint = People.of("Clint Eastwood");
+        var movieGoodBadUgly = Movie.of("The Good, the Bad and the Ugly", 1966);
+        var movieAmericanSniper = Movie.of("American Sniper", 2014);
+        var movieMillion = Movie.of("Million Dollar Baby", 2004);
+        Stream.of(clint, movieMillion, movieAmericanSniper, movieGoodBadUgly)
+                .forEach(entityManager::persist);
+        entityManager.flush();
+        // TODO : add other actors, director Sergio Leone
+        // played movies
+        Collections.addAll(clint.getPlayedMovies(), movieGoodBadUgly, movieMillion);
+        // directed movies
+        Collections.addAll(clint.getDirectedMovies(), movieAmericanSniper, movieMillion);
+        entityManager.flush();
+        // TODO : clear and read person clint again
+    }
 
 }
